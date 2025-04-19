b373a16bf674618aa2c28ae3654479a128d9ce1f
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-19 12:22:00 +0200

    skeleton

---
+++ 1. Primitive Types/striings.py
@@ -0,0 +1,14 @@
+# Simple string
+course = "Python Programming"
+
+# string functions
+# 1. len function checks the size of the string
+print(len(course))
+
+# 2. accessing characters by index
+print(course[0])
+print(course[-1])
+
+# 2.a string slicing using index
+print(course[0:3])
+print(course[0:])
---
+++ 1. Primitive Types/variables.py
@@ -0,0 +1,6 @@
+# used to store data
+students_count = 1000
+rating = 4.99
+is_published = True
+course = "Python Programming"
+print(students_count)
