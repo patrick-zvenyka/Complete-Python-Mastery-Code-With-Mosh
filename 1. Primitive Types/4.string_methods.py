82747c80c42c76e3a9332902881dd5658f5c9ee6
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-20 08:01:51 +0200

    string-methods

---
+++ 1. Primitive Types/4.string_methods.py
@@ -0,0 +1,20 @@
+course = " Python Programming"
+
+# capitalize the whole string
+print(course.upper())
+print(course.lower())
+
+# capitalize the first letters
+print(course.title())
+
+# removes the whitespace in the string
+print(course.strip())
+
+# finds the index in the string
+print(course.find("Pro"))
+
+# replacing a sequence of characters in a string
+print(course.replace("P","J"))
+
+# checking if the series of characters exist
+print("pro" in course)
