9e40cd03e7fe4a0c9a3d86ef9c2a8dce16385d1d
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-20 08:58:11 +0200

    Create 1.comparison_operators.py

---
+++ 2. Control Flow/1.comparison_operators.py
@@ -0,0 +1,21 @@
+# Comparison operators on numbers
+print(10 > 3)
+# True
+
+print(10 >= 3)
+# True
+
+print(10 == '10')
+# False
+
+print(10 != '10')
+# True
+
+# Comparison operators on strings
+print('bag' > 'apple')
+# True
+
+print('bag' == 'BAG')
+# False
+
+print(ord('b'))
