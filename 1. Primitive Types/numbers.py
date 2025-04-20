66ffc92b62e3400dd336fee678c1ecb50f9ef4b8
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-20 08:16:15 +0200

    numbers

---
+++ 1. Primitive Types/numbers.py
@@ -0,0 +1,21 @@
+# types of numbers in Python
+x = 1
+x = 1.1
+x = 1 + 2j
+
+# types of operators in python
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+# augmented assignment operator
+
+x = 10
+x = x + 3
+
+#aap
+x += 3
