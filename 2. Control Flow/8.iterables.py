df9921ae3d4bdaf81cb06bc143acba780e56b592
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-22 09:31:15 +0200

    iterables

---
+++ 2. Control Flow/8.iterables.py
@@ -0,0 +1,11 @@
+print(type(range(4)))
+# the range function is  iterable, can be used in loops
+for x in range(5):
+    print(x)
+
+# strings and lists are iterable too
+for x in "Python":
+    print(x)
+
+for x in [1,2,3,4,5]:
+    print(x)
