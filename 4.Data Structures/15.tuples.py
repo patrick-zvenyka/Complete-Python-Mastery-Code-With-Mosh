2cc113c896c5f978836697b3e5431ace051ecbd3
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2025-09-17 07:03:07 +0200

    tuples

---
+++ 4.Data Structures/15.tuples.py
@@ -0,0 +1,24 @@
+point = 1,2
+print(point, type(point))
+
+# concatenate tuples
+point = (1,2) + (3,4)
+print(point, type(point))
+
+# repeat tuples
+point = (1,2) * 3  
+print(point, type(point))
+
+# convert list to tuple
+point = tuple([1,2,3])
+print(point)
+
+# convert string to tuple
+point = tuple("hello")
+print(point)
+
+# fethch elements
+point = (1,2,3)
+print(point[0])
+print(point[1])
+
